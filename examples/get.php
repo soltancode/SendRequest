aca2897344ecc7d6783b2a0576ab6257f23e76e1
Soltan Abilgasimzada <soltan.ebilqasimzade@gmail.com>
2022-09-30 15:18:35 +0400

    Update examples/get.php

--- examples/get.php
+++ examples/get.php
@@ -1,1 +1,15 @@
 <?php
+
+require '../vendor/autoload.php';
+
+# If you use it as class (facade), import this.
+use Soltancode\SendRequest\Facades\SendRequest;
+
+$baseUrl = "https://dummyjson.com";
+$service = "/products/1";
+
+# Using as class:
+return SendRequest::get($baseUrl, $service);
+
+# Using as helper:
+return sendRequest()->get($baseUrl, $service);
